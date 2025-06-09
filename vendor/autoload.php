00d8b1d6d835701526f55819d053d55c77fb89c6
VEGENUS <115636744+GuillaumeVerliac@users.noreply.github.com>
2025-06-09 22:51:05 +0200

    Create autoload.php

---
+++ vendor/autoload.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__.'/phpmailer/PHPMailer.php';
+require_once __DIR__.'/phpmailer/SMTP.php';
+require_once __DIR__.'/phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
